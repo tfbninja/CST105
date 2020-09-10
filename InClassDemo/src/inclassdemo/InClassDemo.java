250ac63401a58c685ad15af218f7fc797459f8b0
Tim Barber <tfbninja@users.noreply.github.com>
2020-09-10 14:52:06 -0600

    init

---
+++ InClassDemo/src/inclassdemo/InClassDemo.java
@@ -0,0 +1,21 @@
+package inclassdemo;
+
+/**
+ *
+ * @author Tim Barber
+ */
+public class InClassDemo {
+
+    public static void main(String[] args) {
+        System.out.println("\nTim Barber\tSep 10, 2020\tCST-105"); //Header
+        int month, day, year;
+
+        month = 10;
+        day = 26;
+        year = 1969;
+        System.out.println("Here is a date: " + month + "/" + day + "/" + year + ".");
+
+
+    }
+
+}
