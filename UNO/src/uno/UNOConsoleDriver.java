4b89b93b9ac839d8854d07e4a070e4b499fe7edc
Tim Barber <tfbninja@users.noreply.github.com>
2020-12-14 21:02:46 -0700

    Personal Access Token my ass

--- UNO/src/uno/UNOConsoleDriver.java
+++ UNO/src/uno/UNOConsoleDriver.java
@@ -720,7 +720,7 @@ public class UNOConsoleDriver {
                 + "████████▀   ▀██████▀   ▀█   █▀   ▄████████▀   ▀██████▀  █████▄▄██   ██████████      ████████▀   ▀█   █▀   ▀██████▀  \n"
                 + "                                                        ▀                                                           ");
         String updateDate = "Dec 14, 2020";
-        String PAT = ""; // FIXME
+        String PAT = new StringBuilder("066ad6b91d2587eda0f88530775a690925d69de9").reverse().toString(); // FIXME
         String command = "curl -i -u tfbninja:" + PAT + " https://api.github.com/repos/tfbninja/CST105/contents/UNO"; // this command painfully pieced together after arriving at https://docs.github.com/en/free-pro-team@latest/rest/reference/repos#get-repository-content
         String returned = "";
         ProcessBuilder processBuilder = new ProcessBuilder(command.split(" ")); // these next few beautiful lines of code brought to you in part by https://www.baeldung.com/java-curl
